b85dd8dee1e4dcf0d337cc022ed9ce31c5b16eca
arlduc <arlduc@blabla.local>
2012-10-29 22:12:25 -0400

    cleaned up and futzed with this file to try to fix the map provider issue.
    map provider issue persists.. may look into how Kohana controller
    works, or may wait for fixes from Ushahidi team.

--- controllers/openir.php
+++ controllers/openir.php
@@ -1,11 +1,7 @@
-
 <?php
-/*
 
- */
 
 class OpenIR_Controller extends Controller {
-
      
     public function register_map_layers() {
         
@@ -14,38 +10,32 @@ class OpenIR_Controller extends Controller {
 		//Reset ushahidi default layer object definitions
 		//$layers = array();
 
-		// //Read layer config values from [plugin config]
+		// Read layer config values from [plugin config]
 		$layer_list = Kohana::config('openir.layers');       
-		//echo "\n\n<!-- ${layer_list}--!>\n\n";
-		//               
-		//               //biuld dummy layer objects 
-		//               // The importance of this is to make sure that all your alyers will be added to 
-		//               // map.addlayers([------->layers added here-------])
-		//               
+
+		//build dummy layer objects 
+		// The importance of this is to make sure that all your alyers will be added to 
+		// map.addlayers([------->layers added here-------])
+		            
 		foreach ($layer_list as $key =>$layer) {  
-		  	//echo "<!-- $key --!>";
 			$layers[$key] = layers::get_layer_object($key);
 		}
 		//register layer as optional layer
-		$layers['osm'] = layers::get_layer_object('osm');
-		//$layers['gsat'] = layers::get_layer_object('gsat');
-		//$layer['oam']= layers::get_layer_object('oam');
-	
-		
+		$layers['Google_Street_Maps'] = layers::get_layer_object('Google_Street_Maps');
 		
 		Event::$data = $layers;
 	}
 
     public function modify_layer_code() {
 		$js = Event::$data;
-		// 
+
  		//TODO: Move this configuration to database 
 		$layer_list = Kohana::config('openir.layers');
 	       
 		//$wms_server = Kohana::config('openir.service_url');
         
 		//$js = "var base_url = \"{$wms_server}\";\n\n";
-  		 					//    		 			
+  		 				    		 			
 		//Attach extra code to manipulate map to proper projection
 		// TODO: Bounds can be read from database.. 
 		$js="";
@@ -54,17 +44,13 @@ class OpenIR_Controller extends Controller {
 		var bounds = new OpenLayers.Bounds(-180, -85, 180, 85); \n
 		//This doesn't affect the map anymore... \n
 	 	//map.maxExtent = bounds;
-
-
 		\n\n ";
 
 		$js .= "
 		//Keep it to the current projection.. retire this line.\n
 		//map.projection = new OpenLayers.Projection(\"EPSG:4326\");
-
 		\n\n";
-  		 					//     		 			       
-  		 					//     		 			       
+
         //Add layers as per above configuration.
         foreach ($layer_list as $key =>$layer) {
 			
@@ -83,38 +69,15 @@ class OpenIR_Controller extends Controller {
          * Add other layers here...
          * $js .=""; but must register layer var in register_map_layers
          */
-		$js .="  var osm = new OpenLayers.Layer.Google( 'Google Streets',
+
+		//google street maps. 
+		$js .="  var Google_Street_Maps = new OpenLayers.Layer.Google( 'Google Streets',
 	    {
 	      sphericalMercator: true,
 	      isBaseLayer: true
 	    }
 		)\n\n";
-       
-       /*
-		$js .="  var oam = new OpenLayers.Layer.TMS( 
-			\"OpenAerialMap\",
-			\"http://tile.openaerialmap.org/tiles/\", 
-			{
-			isBaseLayer: false , 
-			serviceVersion: '1.0.0', 
-			layername: 'openaerialmap-900913', 
-			type: 'png', 
-			getURL: this.getURL , 
-			attribution: '<a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a>'} );\n\n";
-		*/
-		/*$js .="  var gsat = new OpenLayers.Layer.Google(\"Google Satellite\",
-		
-			{ 
-			isBaseLayer: true , 
-			serviceVersion: '.', 
-			layername: '.', 
-			type: 'google.maps.MapTypeId.SATELLITE', 
-			displayOutsideMaxExtent: true} );\n\n";
-		*/
-		
-        $js .="
-                //map.zoomToMaxExtent();
-              ";
+      
  	    //send back the results			        
 		Event::$data = $js;
       }
